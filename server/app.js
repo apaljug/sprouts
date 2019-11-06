fc665e89e100213ebd4a619334a1c0083053df0e
Andreas Paljug <andreaspaljug@gmail.com>
2019-11-06 01:06:52 -0500

    Setup firebase admin intialization with server.

--- server/app.js
+++ server/app.js
@@ -4,11 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
-
 var app = express();
+var admin = require('./firebase-service/admin');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/users/me", usersRouter);
 
 
 // catch 404 and forward to error handler
@@ -40,5 +42,25 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+async function verifyToken(req, res, next){
+  const idToken = req.headers.authorization;
+
+  try {
+    const decodedToken = admin.auth().verifyIdToken(idToken);
+
+    if (decodedToken) {
+      req.body.uid = decodedToken.uid;
+
+      return next();
+    } else {
+      return res.status(401).send("You are not authorization! :(");
+    }
+  } catch (e) {
+      return res.status(401).send("You are not authorization at all! :(");
+  }
+}
+
+app.use('/', verifyToken);
 
 module.exports = app;
